e0ff6595908aa0b1f0e63d39d7ed1773a6a0fb8b
ahmed-madkour1 <ahmed.madkour1@vodafone.com>
2025-05-09 17:42:02 +0300

    Revert "clear main branch"
    This reverts commit 778c78e0279a1f6f946ba9f6d042defcbd6c96b3.

---
+++ src/BankingSystem.java
@@ -0,0 +1,8 @@
+public interface BankingSystem {
+     void bankServices(Bank bank);
+     void createAccount();
+     void deposit(Bank bank);
+     void withdraw(Bank bank);
+     void viewBalance(Bank bank);
+
+}
